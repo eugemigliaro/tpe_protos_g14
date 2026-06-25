46412d4525212f773fef4a3902549cbcfef20b18
eugemigliaro <eugenio.migliaro@gmail.com>
2026-06-25 18:59:19 -0300

    integrar toolkit de catedra y parser de args; reubicar a src/server/utils y test/
    - utilidades de catedra (buffer, netutils, parser, parser_utils, selector, stm)
      movidas de src/ a src/server/utils/ (atribucion en la historia via git am)
    - archivos *_test.c, tests.h y foo.dot movidos a test/ (fuera del build principal)
    - parser de argumentos oficial (args.c/args.h) integrado en src/server, usado por server.c
    - Makefile.inc: -Isrc/server/utils
    
    Build OK con make all (2 warnings benignos en codigo de catedra bajo -Wextra).

--- src/server/server.c
+++ src/server/server.c
@@ -1,36 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 
+#include "args.h"
 #include "version.h"
 
-static void usage(const char *prog) {
-    fprintf(stderr,
-        "Uso: %s [-p PUERTO] [-P PUERTO_CONF] [-v] [-h]\n"
-        "  -p PUERTO        puerto SOCKS5 (default 1080)\n"
-        "  -P PUERTO_CONF   puerto del protocolo de monitoreo (default 8080)\n"
-        "  -v               imprime version y termina\n"
-        "  -h               imprime esta ayuda y termina\n",
-        prog);
-}
-
 int main(int argc, char *argv[]) {
-    unsigned short socks_port = 1080;
-    unsigned short conf_port = 8080;
+    struct socks5args args;
+    parse_args(argc, argv, &args); /* maneja -h/-v y termina si corresponde */
 
-    int opt;
-    while ((opt = getopt(argc, argv, "p:P:vh")) != -1) {
-        switch (opt) {
-            case 'p': socks_port = (unsigned short) atoi(optarg); break;
-            case 'P': conf_port  = (unsigned short) atoi(optarg); break;
-            case 'v': printf("server %s\n", version_string()); return 0;
-            case 'h': usage(argv[0]); return 0;
-            default:  usage(argv[0]); return 1;
-        }
-    }
-
-    printf("[scaffold] server %s — SOCKS5 puerto %u, monitoreo puerto %u\n",
-           version_string(), socks_port, conf_port);
+    printf("[scaffold] server %s\n", version_string());
+    printf("[scaffold]   SOCKS5     %s:%u\n", args.socks_addr, args.socks_port);
+    printf("[scaffold]   monitoreo  %s:%u\n", args.mng_addr, args.mng_port);
+    printf("[scaffold]   disectors  %s\n", args.disectors_enabled ? "on" : "off");
     printf("[scaffold] TODO: event loop no bloqueante + maquina de estados SOCKS5 (Fase 1).\n");
     return 0;
 }
